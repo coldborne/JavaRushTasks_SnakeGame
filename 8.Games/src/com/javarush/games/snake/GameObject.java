225249887103b2f4965e79ad9869e13345faf2d0
coldborne <slava2232162012@gmail.com>
2022-07-29 02:18:29 +0300

    SnakeGame Задание 3
    Был переделан класс Main, теперь это класс GameObject, который хранит координаты поля, передаваемые через конструктор. Также в нём были реализованы get и set методы на будущее

---
+++ 8.Games/src/com/javarush/games/snake/GameObject.java
@@ -0,0 +1,27 @@
+package com.javarush.games.snake;/*Main method*/
+
+public class GameObject {
+    public int x = 0;
+    public int y = 0;
+
+    GameObject(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX(){
+        return this.x;
+    }
+
+    public void setX(int x){
+        this.x = x;
+    }
+
+    public int getY(){
+        return this.y;
+    }
+
+    public void setY(int y){
+        this.y = y;
+    }
+}
--- 8.Games/src/com/javarush/games/snake/Main.java
+++
@@ -1,7 +0,0 @@
-package com.javarush.games.snake;/*Main method*/
-
-public class Main {
-    public static void main(String[] args) {
-        SnakeGame game = new SnakeGame();
-    }
-}
